0b2a04431521af13bcfcd110ad58551eb87c63bb
Simonbelete <simonbelete@gmail.com>
2021-04-17 12:46:54 +0300

    fixing watch vibe insertation

--- watch.py
+++ watch.py
@@ -7,13 +7,14 @@ from pymongo import MongoClient
 load_dotenv()
 
 
-def watchVibes():
+def watchInsertVibes():
     """ Watch `vibes` collection """
     try:
         print('Connecting to the PostgreSQL databse...')
         mongodbUrl = os.getenv('MONGODB_URL')
         client = MongoClient(mongodbUrl)
-        db = client.ahunbackup
+        #db = client.ahunbackup
+        db = client.ahuntest
 
         # Redis conneciton
         print('Connecting to the Redis databse...')
@@ -29,23 +30,54 @@ def watchVibes():
                 [{'$match': {'operationType': 'insert'}}]
             ):
                 # Conains id of followers
-                followers = []
+                # In order to avoid placing vibe to the posters user vibe treat the current user as a follower
+                followers = [ insert_change['fullDocument']['user'] ]
                 # Append at the top of all followers
                 # Get user's followers
-                for f in db['useredges'].find({'destination': insert_change['user']}):
-                    followers.append(f['_id'])
+                for f in db['useredges'].find({'destination': insert_change['fullDocument']['user']}):
+                    followers.append(f['source'])
                     # Get follower activity type
-                    if db['users'].find({'_id': f['source'], 'interests': {'$in': insert_change['activityType']}}).count() > 0:
-                        r.lpush(str(user['_id']) + ':recommended-high', str(insert_change['_id']))
+                    if db['users'].find({'_id': f['source'], 'interests': {'$in': insert_change['fullDocument'].get('activityType', [])}}).count() > 0:
+                        r.lpush(str(f['source']) + ':recommended-high', str(insert_change['fullDocument']['_id']))
                     else:
-                        r.lpush(str(user['_id']) + ':recommended-medium', str(insert_change['_id']))
+                        r.lpush(str(f['source']) + ':recommended-medium', str(insert_change['fullDocument']['_id']))
 
                 for f in db['users'].find({'_id': {'$nin': followers}}):
-                    r.lpush(str(f['_id']) + ':recommended-medium', str(insert_change['_id']))
+                    r.lpush(str(f['_id']) + ':recommended-reserve', str(insert_change['fullDocument']['_id']))
 
+                print(followers)
+                # TODO: add snapshot in order to restart from the previous watch 
         except pymongo.errors.PyMongoError as ex:
             # TODO: log the execption
             print(ex)
+
+
+def watchDeleteVibes():
+    """ Watch `vibes` collection deletation """
+    try:
+        print('Connecting to the PostgreSQL databse...')
+        mongodbUrl = os.getenv('MONGODB_URL')
+        client = MongoClient(mongodbUrl)
+        #db = client.ahunbackup
+        db = client.ahuntest
+
+        # Redis conneciton
+        print('Connecting to the Redis databse...')
+        r = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'))
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        print(err)
+        sys.exit(1)
+
+    while True:
+        try:
+            pass
+            # TODO: add snapshot in order to restart from the previous watch 
+        except pymongo.errors.PyMongoError as ex:
+            # TODO: log the execption
+            print(ex)
+
+
+
 
 
 def watchUsers():
@@ -87,6 +119,6 @@ def watchUseredges():
 
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
-    executor.submit(watchVibes)
+    executor.submit(watchInsertVibes)
     executor.submit(watchUsers)
     executor.submit(watchUseredges)
